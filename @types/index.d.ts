b5a23b63abad5398e96d07b084245eef23adc4f1
martin-bucinskas <7595909+martin-bucinskas@users.noreply.github.com>
2021-07-06 11:30:23 +0100

    PYI-55-data: Adding some types for the GPG45 engine.
    - Added the identity verification bundle types that the GPG45 engine expects.

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -41,3 +41,110 @@ interface DateInput {
   month: string;
   year: string;
 }
+
+namespace GPG45 {
+  export enum EvidenceType {
+    UK_PASSPORT = "UK_PASSPORT",
+    ATP_GENERIC_DATA = "ATP_GENERIC_DATA"
+  }
+
+  export interface EvidenceData {
+    data: any;
+  }
+
+  export enum AuthoritativeSource {
+    IPV_ATP_DCS = "urn:di:ipv:atp-dcs",
+    ANY = ""
+  }
+
+  export interface ValidityChecks {
+    original: string;
+    errors: string;
+    details: string;
+    logos: string;
+    consistent: string;
+    authoritativeSource: AuthoritativeSource;
+    visibleSecurityFeatures: string;
+    specialistLightSecurityFeatures: string;
+    physicalSecurityFeatures: string;
+    cryptographicCheck: string;
+    lostStolenCancelled: string;
+    expired: string;
+  }
+
+  export enum Score {
+    NOT_AVAILABLE = 0,
+    ONE = 1,
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4
+  }
+
+  export interface EvidenceScores {
+    strength: Score;
+    validity: Score;
+  }
+
+  export interface IdentityEvidence {
+    uuid: any;
+    type: EvidenceType;
+    evidenceData: EvidenceData;
+    validityChecks: ValidityChecks;
+    evidenceScores: EvidenceScores;
+  }
+
+  export enum IdentityCheck {
+    NONE = "None",
+    PUBLISHED_POLICY = "PublishedPolicy",
+    ANTI_MONEY_LAUNDERING = "AntiMoneyLaundering",
+    PHYSICAL_BIOMETRIC = "PhysicalBiometric"
+  }
+
+  export interface ActivityCheckSource {
+    type: string;
+    identityCheck: IdentityCheck;
+  }
+
+  export interface ActivityCheck {
+    source: ActivityCheckSource;
+    dates: [string];
+  }
+
+  export interface FraudCheck {
+    level: string;
+    fraudIndicators: [any];
+  }
+
+  export enum Quality {
+    LOW = "Low",
+    MEDIUM = "Medium",
+    HIGH = "High"
+  }
+
+  export interface KBV {
+    quality: Quality;
+    question: string;
+    response: string;
+  }
+
+  export interface IdentityVerification {
+    staticKbv: [KBV];
+    photoMatch: string;
+    biometricMatch: string;
+    dynamicKbv: [KBV];
+  }
+
+  export interface BundleScores {
+    activityCheckScore: Score;
+    fraudCheckScore: Score;
+    identityVerificationScore: Score;
+  }
+
+  export interface IdentityVerificationBundle {
+    identityEvidence: [IdentityEvidence];
+    activityChecks: [ActivityCheck];
+    fraudCheck: FraudCheck;
+    identityVerification: IdentityVerification;
+    bundleScores: BundleScores;
+  }
+}
